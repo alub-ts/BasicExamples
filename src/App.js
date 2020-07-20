5df9ed4f357cdc3e010cb70b38f8a141245bc24f
alvin lubetkin <alvin@terminal.co>
2020-07-20 15:29:03 -0400

    for james

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  TerminalHttpProvider,
-  SourceType,
-  EnvironmentTypes
-} from "@terminal-packages/sdk";
 import Web3 from "web3";
 
-const web3 = new Web3(window.terminal.ethereum);
+const web3Provider = window.ethereum;
+const web3 = new Web3(web3Provider);
 
 function App() {
   const sendTx = () => {
-    web3.eth.getAccounts().then(accounts => {
+    web3.eth.getAccounts().then((accounts) => {
       console.log(accounts);
+
       web3.eth
         .sendTransaction({
           from: accounts[0],
-          to: accounts[0],
-          value: web3.utils.toWei(".01", "ether")
+          to: accounts[0], //sends to itself
+          value: web3.utils.toWei(".01", "ether"),
         })
         .then(console.log);
     });
   };
+
+  const verify = () => {
+    web3Provider.enable();
+    console.log("signed in");
+  };
+
   const test = () => {
     web3.eth.getBlockNumber().then(console.log);
   };
   return (
     <div className="App">
+      <button onClick={() => verify()}>Enable MM</button>
       <button onClick={() => sendTx()}>Send Tx</button>
       <button onClick={() => test()}>BlockNumber</button>
     </div>
